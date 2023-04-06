0f424038056e59892315f6c9a30ae580cb2d2abe
biscuitrescue <karttikeyasinha11@gmail.com>
2023-04-06 22:52:57 +0530

    packages updated

--- timetable.py
+++ timetable.py
@@ -1,24 +1,42 @@
 #!/usr/bin/env python3
 
-import datetime
+from calendar import day_name
+# from datetime import datetime
 
-x=datetime.datetime.today().weekday()
-x+=1
 
-print()
+days = list(day_name)
 
-days = {
 
-    1:' Monday\n\tNone',
-    2:' Tuesday\n\t5:30 PM - 7:30 PM | Physics',
-    3:' Wednesday\n\t8:15 PM - 10:15 PM | Chemistry',
-    4:' Thursday\n\t4:00 PM - 6:00 PM | Physics',
-    5:' Friday\n\t8:15 PM - 10:15 PM | Chemistry',
-    6:' Saturday\n\t9:30 AM - 4:00 PM | VMC',
-    7:' Sunday\n\t9:30 AM - 4:00 PM | VMC',
+class Day:
+    def __init__(self, day):
+        self.day = day
+        self.id = days.index(self.day)
 
-}
 
-for i in range(len(days)):
-    if i==x:
-        print(days[x])
+today = Day()
+
+
+# x=datetime.datetime.today().weekday()
+# x+=1
+#
+# print()
+#
+# days = {
+#
+#     1:' Monday\n\tNone',
+#     2:' Tuesday\n\t5:30 PM - 7:30 PM | Physics',
+#     3:' Wednesday\n\t8:15 PM - 10:15 PM | Chemistry',
+#     4:' Thursday\n\t4:00 PM - 6:00 PM | Physics',
+#     5:' Friday\n\t8:15 PM - 10:15 PM | Chemistry',
+#     6:' Saturday\n\t9:30 AM - 4:00 PM | VMC',
+#     7:' Sunday\n\t9:30 AM - 4:00 PM | VMC',
+#
+# }
+#
+# for i in range(len(days)):
+#     if i==x:
+#         print(days[x])
+
+class Day:
+    def __init__(self, index):
+        self.index = index
