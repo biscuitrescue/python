64dacca61fd21584f9f410b335503e9683e78bee
biscuitrescue <karttikeyasinha11@gmail.com>
2023-04-07 04:15:01 +0530

    todo

--- timetable.py
+++ timetable.py
@@ -2,42 +2,56 @@
 
 from calendar import day_name
 from datetime import datetime
-from os.path import realpath
+from os.path import realpath, exists
+import pickle
 
 
 days = list(day_name)
+
+file = realpath(__file__)
+file = file[:file.rfind('/')+1]+'content.txt'
 
 
 class Day:
     def __init__(self, day):
         self.day = day
         self.id = days.index(self.day)
-        self.content = list()
+        if exists(file):
+            self.content = self.read_from_file()
+        else:
+            self.content = list()
 
     def add_single(self, content):
-        self.content.append(content)
+        if content not in self.content:
+            self.content.append(content)
 
     def add_multiple(self, content):
-        self.content.extend(content)
+        if not any(i in self.content for i in content):
+            self.content.extend(content)
+
+    def read_from_file(self):
+        with open(file, 'rb') as f:
+            x = pickle.load(f)
+        return x
 
     def write_to_file(self):
-        file = realpath(__file__)
-        for i in range(len(file)-1, 0, -1):
-            if file[i] == '/':
-                file = file[:i+1]
-                break
+        with open(file, 'wb') as f:
+            pickle.dump(self.content, f)
 
-        print(file[0:len(__file__)])
-
-    def del_content(self, content):
-        self.content.remove(content)
+    def del_content(self, selected):
+        if selected == 'all':
+            i = len(self.content)-1
+        for i in range(len(self.content)):
+            if i in selected:
+                del self.content[i]
 
     def __str__(self):
         return f"{self.day}:\n\t work to be done: {[i for i in self.content]}"
 
 
 today = Day(datetime.now().strftime("%A"))
-today.write_to_file()
+today.add_multiple(("hehe", "hi"))
+today.del_content('all')
 
 # x+=1
 #
