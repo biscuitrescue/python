9b9a7b844a9d1b7ee851448e7eceae05f6fdaea3
biscuitrescue <karttikeyasinha11@gmail.com>
2023-04-07 00:53:50 +0530

    todo

--- timetable.py
+++ timetable.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 from calendar import day_name
-# from datetime import datetime
+from datetime import datetime
+from os.path import realpath
 
 
 days = list(day_name)
@@ -11,12 +12,33 @@ class Day:
     def __init__(self, day):
         self.day = day
         self.id = days.index(self.day)
+        self.content = list()
+
+    def add_single(self, content):
+        self.content.append(content)
+
+    def add_multiple(self, content):
+        self.content.extend(content)
+
+    def write_to_file(self):
+        file = realpath(__file__)
+        for i in range(len(file)-1, 0, -1):
+            if file[i] == '/':
+                file = file[:i+1]
+                break
+
+        print(file[0:len(__file__)])
+
+    def del_content(self, content):
+        self.content.remove(content)
+
+    def __str__(self):
+        return f"{self.day}:\n\t work to be done: {[i for i in self.content]}"
 
 
-today = Day()
+today = Day(datetime.now().strftime("%A"))
+today.write_to_file()
 
-
-# x=datetime.datetime.today().weekday()
 # x+=1
 #
 # print()
@@ -37,6 +59,3 @@ today = Day()
 #     if i==x:
 #         print(days[x])
 
-class Day:
-    def __init__(self, index):
-        self.index = index
