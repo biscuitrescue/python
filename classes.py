967b4e3b353b43ac85e5b769779e8498fcf52963
biscuitrescue <karttikeyasinha11@gmail.com>
2023-04-06 18:18:26 +0530

    todo

--- classes.py
+++ classes.py
@@ -1,5 +1,28 @@
 #!/usr/bin/env python3
 
+
+class Topic:
+    def __init__(self, name, deadline):
+        self.deadline = deadline
+        self.name = name
+        self.done = list()
+        self.left = list()
+
+        self.left.append(self.name)
+
+        def finished_topic(self):
+            self.left.remove(self.name)
+
+
 class Subject:
-    def __init__():
-        pass
+    def __init__(self, name):
+        self.all = dict()
+        self.name = name
+
+    def add_to_subject(self, name, deadline):
+        if name not in self.all.keys:
+            new_topic = Topic(name, deadline)
+            self.all.update({self.name: new_topic})
+            return 'Added'
+        else:
+            return 'Already here?!?'
